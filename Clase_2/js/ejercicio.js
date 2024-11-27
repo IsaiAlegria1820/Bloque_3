2733d91e4506358495b0400a4b4e31403680e47a
Ivan Alegria <iiaj1005@gmail.com>
2024-11-26 19:45:17 -0600

    planteamiento del ejercicio de la clase

---
+++ Clase_2/js/ejercicio.js
@@ -0,0 +1,16 @@
+/*
+Crea un algroritmo que cuente del 1 al 80 y que indique si el numero es par o impar
+
+requisitos
+1) usar un ciclo for para recorrer del 1 al 20
+2) utiliza una condicional para verificar si es par o impar
+3) imprimir el numero con el mensaje indicando si es par o impar
+
+Salida esperada
+1 es impar
+2 es par
+3 es impar
+4 es par
+...
+listo sir o lady
+*/
