f49e81bcb08c56b07b3d6dabf8d1e8e469497729
Ivan Alegria <iiaj1005@gmail.com>
2024-11-26 18:55:34 -0600

    ejemplo de estructura de programacion basica

---
+++ Clase_2/js/control.js
@@ -0,0 +1,23 @@
+/*
+Calcular area de un triangul0
+
+base
+altura
+
+figura
+
+ingresa base
+ingresa altura
+
+ingresa figura
+
+si figura es igual a cuadrado
+    base * altura
+si figura es igual a triangulo
+    base * altura / 2
+
+de otro modo si figura es diferente de trianquglo y cuadrado
+    figura no es un triangulo ni un cuadrado
+
+
+*/
