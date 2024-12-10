fcf0529095048d0e20587f47a0c0d273395408c6
Ivan Alegria <iiaj1005@gmail.com>
2024-12-09 21:54:33 -0600

    completando ejercicio 4,

--- Ejercicios_Campus/Leccion_4/js/ejercicio.js
+++ Ejercicios_Campus/Leccion_4/js/ejercicio.js
@@ -1,16 +1,32 @@
 let librosLeidos=[];
 
 function agregarLibro(titulo){
-    titulo= document.getElementById('tituloLibro');
-    librosLeidos.push=titulo;
+     const libro={
+        id: Date.now(),
+        titulo
+     }; 
+    librosLeidos.push(libro);
+    console.log(librosLeidos)
+    mostrarLibrosLeidos();
 }
 
 function mostrarLibrosLeidos(){
-   
+   const listaTitulos= document.getElementById('listaTitulos');
+   listaTitulos.innerHTML="";
+   librosLeidos.forEach(libro =>{
+    const listItem= document.createElement('li');
+    listItem.innerHTML=`
+    <div>
+        <h5>${libro.titulo}</h5>
+    </div>
+    `;
+    listaTitulos.appendChild(listItem);
+    })   
 }
 
 document.getElementById('formLibreria').addEventListener("submit", function(event){
     event.preventDefault();
-    agregarLibro();
-    mostrarLibrosLeidos();
+    const titulo=document.getElementById("tituloLibro").value;
+    agregarLibro(titulo);
+    event.target.reset()
 });
